7e41246aeb0a364d41c26ff018303c1a4f717448
jonathan <jonathan.taws@gmail.com>
2016-03-11 14:44:13 +0100

    Updated regex with re2

--- src/TestRegex.cpp
+++ src/TestRegex.cpp
@@ -5,34 +5,87 @@
 #include "TestRegex.h"
 
 void testRegex () {
-    //boost::regex r("(st|mt)|tr");
-//    cerr << "st|mt|tr" << " matches st? " << regex_match("st", r) << endl;
-//    cerr << "st|mt|tr" << " matches mt? " << regex_match("mt", r) << endl;
-//    cerr << "st|mt|tr" << " matches tr? " << regex_match("tr", r) << endl;
+
+    RE2 REG_VAR("^(var )");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_VAR !" << std::endl;
+    }
+    RE2 REG_CONST("^(const )");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_CONST !" << std::endl;
+    }
+    RE2 REG_LIRE("^(lire )");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_LIRE !" << std::endl;
+    }
+    RE2 REG_ECRIRE("^(ecrire )");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_ECRIRE !" << std::endl;
+    }
+    RE2 REG_IDENTIFICATEUR("(^[a-zA-Z](\\w)*)");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_IDENTIFICATEUR !" << std::endl;
+    }
+    RE2 REG_FIN_INSTRUCTION("^(;)$");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_FIN_INSTRUCTION !" << std::endl;
+    }
+    RE2 REG_NOMBRE("^(-?\\d+(\\.\\d*)?)");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_NOMBRE !" << std::endl;
+    }
+    RE2 REG_OP_PLUS("^(\\+)");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_OP_PLUS !" << std::endl;
+    }
+    RE2 REG_OP_MOINS("^(\\-)");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_OP_MOINS !" << std::endl;
+    }
+    RE2 REG_OP_DIVISER("^(\\\\)");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_OP_DIVISER !" << std::endl;
+    }
+    RE2 REG_OP_MULTIPLIER("^(\\*)");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_OP_MULTIPLIER !" << std::endl;
+    }
+    RE2 REG_OP_AFFECTER("^(:=)");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_OP_AFFECTER !" << std::endl;
+    }
+    RE2 REG_OP_DECLARER("^(=)");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_OP_DECLARER !" << std::endl;
+    }
+    RE2 REG_PARENTHESE_OUVRANTE("^(\\()");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_PARENTHESE_OUVRANTE !" << std::endl;
+    }
+    RE2 REG_PARENTHESE_FERMANTE("^(\\))");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_PARENTHESE_FERMANTE !" << std::endl;
+    }
+    RE2 REG_SEPARATEUR_DECLARATION("^(,)");
+    if(!REG_VAR.ok()) {
+        std::cerr << "Problem initializing REG_SEPARATEUR_DECLARATION !" << std::endl;
+    }
+
+    std::string captured;
+    RE2::PartialMatch("1re2;", REG_IDENTIFICATEUR, &captured);
+    std::cout << captured << std::endl;
 
     assert(RE2::FullMatch("hello", "h.*o"));
     assert(!RE2::FullMatch("hello", "e"));
 
-    // Regex for the project
-    //boost::regex REG_VAR("(var )");
-//    std::string var = "var ";
-//    boost::smatch match;
-//    std::cout << "Match : " << regex_match(var, match, REG_VAR);
+    std::string text[] = { "While", "I", "pondered,", "weak", "and", "weary" };
 
-//    regex REG_CONST("^const ");
-//    regex REG_LIRE("^lire ");
-//    regex REG_ECRIRE("^ecrire ");
-//    regex REG_IDENTIFICATEUR("^[a-zA-Z](\\w)*");
-//    regex REG_FIN_INSTRUCTION("^;$");
-//    regex REG_NOMBRE("^-?\\d+(\\.\\d*)?");
-//    regex REG_OP_PLUS("^\\+");
-//    regex REG_OP_MOINS("^\\-");
-//    regex REG_OP_DIVISER("^\\\\");
-//    regex REG_OP_MULTIPLIER("^\\\\");
-//    regex REG_OP_AFFECTER("^:=");
-//    regex REG_OP_DECLARER("^=");
-//    regex REG_PARENTHESE_OUVRANTE("^\\(");
-//    regex REG_PARENTHESE_FERMANTE("^\\)");
-//    regex REG_SEPARATEUR_DECLARATION("^,");
+    for(int i = 0; i < 6; i++) {
+        re2::StringPiece result;
+        if(RE2::PartialMatch(text[i], "([aeiou])", &result))
+            std::cout << "First lower-case vowel at " << result.data() - text[i].data() << " & " << result.data() << " - " << text[i].data() << "\n";
+        else
+            std::cout << "No lower-case vowel\n";
+    }
 
 }
